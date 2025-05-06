93b5001a9acb657a976679ef6d430650d3072cd2
Sara Marques <scmarques99@gmail.com>
2025-05-06 17:40:17 +0200

    fix: description of get nearest resonance function

--- src/resonantstate/simulations_resonance_analysis.py
+++ src/resonantstate/simulations_resonance_analysis.py
@@ -1,11 +1,11 @@
 import numpy as np
 
 
-def get_nearest_resonance(period_ratios, second_order = False, kmax=12, difference_order = 0.5):
-    """Simple function to get the nearest resonance.
+def get_nearest_resonance(period_ratio, second_order = False, kmax=12, difference_order = 0.5):
+    """Simple function to get the nearest resonance for a pair of planets based on their period ratio.
 
     Args:
-        period_ratios (list): list of period ratios between the planets in the system.
+        period_ratio (float): period ratio between a pair of planets in the system.
         second_order (bool, optional): if second order resonances are allowed. Defaults to False.
         kmax (int, optional): maximum k value to be checked. Defaults to 12.
 
@@ -15,13 +15,13 @@ def get_nearest_resonance(period_ratios, second_order = False, kmax=12, differen
     
     # Get closest first order resonance
     ks = np.arange(2, kmax)
-    possible_deltas = period_ratios * (ks - 1) / ks - 1
+    possible_deltas = period_ratio * (ks - 1) / ks - 1
     k = ks[np.argmin(np.abs(possible_deltas))]
     min_delta = np.min(np.abs(possible_deltas))
     
     # Get closest second order resonance
     if second_order:
-        possible_deltas = period_ratios * (ks - 2) / ks - 1
+        possible_deltas = period_ratio * (ks - 2) / ks - 1
         k2 = ks[np.argmin(np.abs(possible_deltas))]
         min_delta2 = np.min(np.abs(possible_deltas))
         
